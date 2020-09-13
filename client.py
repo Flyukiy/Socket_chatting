af36a3805a9a1045a61b2912f3eceb863cbdf38d
Flyukiy <kamoliddin7usmonov@gmail.com>
2020-09-14 07:32:25 +0900

    new

--- client.py
+++ client.py
@@ -1,60 +1,27 @@
-# chat_client.py
+import socket, threading
+nickname = input("Choose your nickname: ")
 
-import sys
-import socket
-import select
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      #socket initialization
+client.connect(('127.0.0.1', 7971))                             #connecting client to server
 
-
-def chat_client():
-    if(len(sys.argv) < 3):
-        print 'Usage : python chat_client.py hostname port'
-        sys.exit()
-
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-
-    # connect to remote host
-    try:
-        s.connect((host, port))
-    except:
-        print 'Unable to connect'
-        sys.exit()
-
-    print 'Connected to remote host. You can start sending messages'
-    sys.stdout.write('[Me] ')
-    sys.stdout.flush()
-
-    while 1:
-        socket_list = [sys.stdin, s]
-
-        # Get the list sockets which are readable
-        ready_to_read, ready_to_write, in_error = select.select(
-            socket_list, [], [])
-
-        for sock in ready_to_read:
-            if sock == s:
-                # incoming message from remote server, s
-                data = sock.recv(4096)
-                if not data:
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else:
-                    # print data
-                    sys.stdout.write(data)
-                    sys.stdout.write('[Me] ')
-                    sys.stdout.flush()
-
+def receive():
+    while True:                                                 #making valid connection
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('ascii'))
             else:
-                # user entered a message
-                msg = sys.stdin.readline()
-                s.send(msg)
-                sys.stdout.write('[Me] ')
-                sys.stdout.flush()
+                print(message)
+        except:                                                 #case on wrong ip/port details
+            print("An error occured!")
+            client.close()
+            break
+def write():
+    while True:                                                 #message layout
+        message = input(f'{nickname}: ')
+        client.send(message.encode('ascii'))
 
-
-if __name__ == "__main__":
-
-    sys.exit(chat_client())
+receive_thread = threading.Thread(target=receive)               #receiving multiple messages
+receive_thread.start()
+write_thread = threading.Thread(target=write)                   #sending messages 
+write_thread.start()
--- server.py
+++ server.py
@@ -1,87 +1,45 @@
-# chat_server.py
+import socket, threading                                                #Libraries import
 
-import sys
-import socket
-import select
+host = '127.0.0.1'                                                      #LocalHost
+port = 7971                                                             #Choosing unreserved port
 
-HOST = ''
-SOCKET_LIST = []
-RECV_BUFFER = 4096
-PORT = 9009
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)              #socket initialization
+server.bind((host, port))                                               #binding host and port to socket
+server.listen()
 
-def chat_server():
+clients = []
+nicknames = []
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(10)
+def broadcast(message):                                                 #broadcast function declaration
+    for client in clients:
+        client.send(message)
 
-    # add server socket object to the list of readable connections
-    SOCKET_LIST.append(server_socket)
+def handle(client):                                         
+    while True:
+        try:                                                            #recieving valid messages from client
+            message = client.recv(1024)
+            broadcast(message)
+        except:                                                         #removing clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
 
-    print "Chat server started on port " + str(PORT)
+def receive():                                                          #accepting multiple clients
+    while True:
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))       
+        client.send('NICKNAME'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!\n".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
 
-    while 1:
-
-        # get the list sockets which are ready to be read through select
-        # 4th arg, time_out  = 0 : poll and never block
-        ready_to_read, ready_to_write, in_error = select.select(
-            SOCKET_LIST, [], [], 0)
-
-        for sock in ready_to_read:
-            # a new connection request recieved
-            if sock == server_socket:
-                sockfd, addr = server_socket.accept()
-                SOCKET_LIST.append(sockfd)
-                print "Client (%s, %s) connected" % addr
-
-                broadcast(server_socket, sockfd,
-                          "[%s:%s] entered our chatting room\n" % addr)
-
-            # a message from a client, not a new connection
-            else:
-                # process data recieved from client,
-                try:
-                    # receiving data from the socket.
-                    data = sock.recv(RECV_BUFFER)
-                    if data:
-                        # there is something in the socket
-                        broadcast(server_socket, sock, "\r" +
-                                  '[' + str(sock.getpeername()) + '] ' + data)
-                    else:
-                        # remove the socket that's broken
-                        if sock in SOCKET_LIST:
-                            SOCKET_LIST.remove(sock)
-
-                        # at this stage, no data means probably the connection has been broken
-                        broadcast(server_socket, sock,
-                                  "Client (%s, %s) is offline\n" % addr)
-
-                # exception
-                except:
-                    broadcast(server_socket, sock,
-                              "Client (%s, %s) is offline\n" % addr)
-                    continue
-
-    server_socket.close()
-
-# broadcast chat messages to all connected clients
-
-
-def broadcast(server_socket, sock, message):
-    for socket in SOCKET_LIST:
-        # send the message only to peer
-        if socket != server_socket and socket != sock:
-            try:
-                socket.send(message)
-            except:
-                # broken socket connection
-                socket.close()
-                # broken socket, remove it
-                if socket in SOCKET_LIST:
-                    SOCKET_LIST.remove(socket)
-
-
-if __name__ == "__main__":
-
-    sys.exit(chat_server())
+receive()
